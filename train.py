3eb7c1d5d15a0d3a91581e03d63c8731ebeeeb49
NidhoggD1 <nidhoggd1@gmail.com>
2024-07-09 23:05:33 +0800

    add parameters

--- train.py
+++ train.py
@@ -3,3 +3,13 @@ from ultralytics import YOLO
 model = YOLO('./pretrained_models/yolov8s.pt')
 
 model.info()
+
+# Train the model
+# Some arbitrary values
+model.train(data='./data.yaml',
+            epochs=100,
+            batch_size=32,
+            img_size=640,
+            learning_rate=0.001,
+            optimizer='Adam',
+            lr_scheduler='cosine')
